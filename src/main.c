c17d55174584caf56b7e8f59422f72d1b20fed02
Cyrus Wilkie <james.wilkiec@gmail.com>
2021-05-24 11:26:52 +1000

    added main skeleton

---
+++ src/main.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+   $$$$$\ $$\                                        $$$$$$\            $$\           
+   \__$$ |\__|                                      $$  __$$\           \__|          
+      $$ |$$\ $$$$$$\$$$$\  $$$$$$\$$$$\  $$\   $$\ $$ /  \__| $$$$$$\  $$\ $$$$$$$\  
+      $$ |$$ |$$  _$$  _$$\ $$  _$$  _$$\ $$ |  $$ |$$ |      $$  __$$\ $$ |$$  __$$\ 
+$$\   $$ |$$ |$$ / $$ / $$ |$$ / $$ / $$ |$$ |  $$ |$$ |      $$ /  $$ |$$ |$$ |  $$ |
+$$ |  $$ |$$ |$$ | $$ | $$ |$$ | $$ | $$ |$$ |  $$ |$$ |  $$\ $$ |  $$ |$$ |$$ |  $$ |
+\$$$$$$  |$$ |$$ | $$ | $$ |$$ | $$ | $$ |\$$$$$$$ |\$$$$$$  |\$$$$$$  |$$ |$$ |  $$ |
+ \______/ \__|\__| \__| \__|\__| \__| \__| \____$$ | \______/  \______/ \__|\__|  \__|
+                                          $$\   $$ |                                  
+                                          \$$$$$$  |                                  
+                                           \______/                                   
+
+This is the proof of concept prototype for JimmyCoin, created to establish basic cryptography
+and mining for the blockchain without implementing the brain bending network stuff. It acts
+as the wallet, node and miner for every transaction, the end goal is to split up the functions
+within main into individual programs which can be distributed, with data transferred via a P2P network.
+*/
+
+/*
+The main function for my concept prototype
+*/
+int main(int argc, char *argv[]) {
+    // Simulates wallet sending out transaction
+    char *transaction = runClient();
+    // Simulates a node checking the transaction is valid
+    int valid = validateTransaction(transaction);
+
+    char *newBlock = NULL;
+
+    // Simulating a mining node
+    if (valid) {
+        addToPool(transaction);
+        newBlock = mineBlock();
+    }
+
+    // Node receives block, validates it and adds it to the chain 
+    validateBlock(newBlock);
+}
